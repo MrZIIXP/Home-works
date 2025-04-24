227b91c04105928c12f2208cbe66c8ca9faf9411
MrZIIXP <ziyodulloakhrorov@gmail.com>
2025-04-24 12:00:06 +0500

    creat git project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      md:{max: "510px"},
+
+    },
+  },
+  plugins: [],
+}
+
